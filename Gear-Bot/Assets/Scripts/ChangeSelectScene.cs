c3ebf0c04f6ed922707c11dcad4b228768874d0f
yuiTan204 <ktc23a31a0017@edu.kyoto-tech.ac.jp>
2025-09-22 19:22:27 +0900

    StageSelectSceneでMapを選択する際に選んでいるMapが拡大する

--- Gear-Bot/Assets/Scripts/ChangeSelectScene.cs
+++ Gear-Bot/Assets/Scripts/ChangeSelectScene.cs
@@ -9,6 +9,8 @@ public class ChangeSelectScene : MonoBehaviour
     [SerializeField] private GameObject StageCanvas1;
     [SerializeField] private GameObject StageCanvas2;
 
+    [SerializeField] private float value = 1.5f;
+
     private void Start()
     {
         StageCanvas1.SetActive(true);
@@ -25,5 +27,15 @@ public class ChangeSelectScene : MonoBehaviour
     {
         StageCanvas1.SetActive(true);
         StageCanvas2.SetActive(false);
+    }
+
+    public void OnClickExpansionImage(GameObject islandObj)
+    {
+        islandObj.GetComponent<RectTransform>().localScale *= value;
+    }
+
+    public void OnClickReductionImage(GameObject islandObj)
+    {
+        islandObj.GetComponent<RectTransform>().localScale /= value;
     }
 }
